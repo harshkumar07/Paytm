a864e41d9cbf27a87ff0a761e8194c7b03eea972
harshkumar07 <harshku068@gmail.com>
2024-03-21 15:48:48 +0530

    testing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     <Routes>
       <Route path ="/signup" element={<Signup/>} />
       <Route path ="signin" element={<Signin/>} />
-      <Route path ="/dashboard" element={<Dashboard/>} />
+      <Route path ="/" element={<Dashboard/>} />
       <Route path ="/send" element={<SendMoney/>} />
       <Route path ="/profile" element={<Profile/>} />
       <Route path ="/update" element={<Update/>} />
